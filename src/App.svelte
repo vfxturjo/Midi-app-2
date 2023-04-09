c298066a795846157545528998b6b6c049adbd24
vfxTurjo <vfxturjo@gmail.com>
2023-04-09 20:32:30 +0600

    hothat commit

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
 // import JsoNinterface2 from "./lib/JSON interface 2/JSON interface 2.svelte";
 // import Apatotoapp from "./lib/JSON interface/apatoto app.svelte";
 // import MachineLoader from "./lib/testing/MachineLoader.svelte";
-import Testing3 from "./lib/testing3/testing 3.svelte";
 // ! create main interface
 // import AvailableEvents from "./lib/Available events/available events.svelte";
 </script>
@@ -14,7 +13,6 @@ import Testing3 from "./lib/testing3/testing 3.svelte";
   <!-- CURRENTLY WORKING-->
 
   <!-- <Testing2 /> -->
-  <Testing3 />
 
   <!-- <Apatotoapp /> -->
   <!-- <MachineLoader /> -->
